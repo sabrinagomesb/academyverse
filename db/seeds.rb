e528bb9e75ce29216d3793effb50a77078f5fa70
Sabrina Gomes <sabrinagbdev@gmail.com>
2024-06-17 22:16:16 -0300

    feat: add courses data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# create using gem faker 10 courses
+
+puts 'Creating courses...'
+10.times do
+  Course.create(
+    title: Faker::Educator.course_name,
+    description: Faker::Lorem.paragraph,
+  )
+end
+puts 'Courses created!'
