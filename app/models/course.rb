81362c902c155689ee559abc52b0dd9e1eba9094
Sabrina Gomes <sabrinagbdev@gmail.com>
2024-06-21 21:14:51 -0300

    feat: add and config active storage

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,7 @@
 class Course < ApplicationRecord
+
+  has_one_attached :image do |attachable|
+    # attachable.variant :thumb, resize: "100x100"
+    attachable.variant :thumb, resize_to_limit: [100, 100]
+  end
 end
